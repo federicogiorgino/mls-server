bd68103f13711c97797357cefb25a0a753aa3653
Federico Giorgino <f_giorgino@icloud.com>
2021-09-16 16:45:52 +0200

    UserModel userSchema.pre('save') method

--- src/models/User.js
+++ src/models/User.js
@@ -21,4 +21,29 @@ const userSchema = new Schema({
   visitors: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
+//runs before saving the user to the database
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    //hash PW
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 mongoose.model("User", userSchema);
